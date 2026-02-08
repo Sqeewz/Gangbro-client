897eccc49f325749f4dbc277834563bdb226cc3a
Sqeewz <dotheripper1@gmail.com>
2026-02-08 16:06:34 +0700

    v2

--- GangBro-client/src/app/app.routes.ts
+++ GangBro-client/src/app/app.routes.ts
@@ -10,11 +10,14 @@ import { AboutMission } from './about-mission/about-mission'
 import { AboutUs } from './about-us/about-us'
 
 
+import { MyMissions } from './missions/my-missions/my-missions'
+
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'login', component: Login },
     { path: 'profile', component: Profile, canActivate: [authGuard], runGuardsAndResolvers: 'always' },
     { path: 'missions', component: Missions, canActivate: [authGuard], runGuardsAndResolvers: 'always' },
+    { path: 'my-mission', component: MyMissions, canActivate: [authGuard] },
     { path: 'about-mission/:id', component: AboutMission, canActivate: [authGuard] },
     { path: 'about-us', component: AboutUs },
 
